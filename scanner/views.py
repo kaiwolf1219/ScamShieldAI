d3c06aa6502ef5cb64317ee8d205acf63fe95f74
kaiwolf1219 <kledbet7@gmail.com>
2026-06-04 14:59:13 -0400

    Built ScamShield homepage and connected scanner app

--- scanner/views.py
+++ scanner/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        return render(request, "submitted.html")
+
+    return render(request, "index.html")
